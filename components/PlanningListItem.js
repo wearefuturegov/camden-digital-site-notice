1a6c4540ad9433792acf16aa167825edcff55d7d
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2021-10-19 18:21:18 +0100

    Add postcode search and calculate distance from planning application locations

--- components/PlanningListItem.js
+++ components/PlanningListItem.js
@@ -1,12 +1,16 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from '../styles/PlanningListItem.module.css'
+import distanceInMiles from '../utils/geolocationHelpers'
 
 const PlanningListItem = (props) => {
-  const { development } = props;
+  const { development, currentLocation } = props;
 
   // The app numbers contain forward slashes, so we replace those with underscores
   // here so it's URL compatible
   const appId = development.application_number.replace(/\//g, "_")
+
+  const distance = currentLocation ? distanceInMiles(currentLocation, development.location) : null;
 
   return (
     <Link href={`/planning-applications/${appId}`}>
@@ -15,7 +19,26 @@ const PlanningListItem = (props) => {
           <div className={styles.imageSpacer}></div>
           <div className={styles.listItemDetails}>
             <h2 className={styles.listItemHeading}>{ development.development_description }</h2>
-            { development.development_address }
+            <div className={styles.listItemLocation}>
+              <div className={styles.locationMarkerWrapper}>
+                <Image src="/location-marker.svg" alt="Distance" width={16} height={25} />
+              </div>
+              <p>
+                { distance &&
+                  <>
+                    <span>
+                      { distance.toFixed(1) } miles
+                    </span>
+                    <span className={styles.spacer}>
+                      •
+                    </span>
+                  </>
+                }
+                <span>
+                  { development.development_address }
+                </span>
+              </p>
+            </div>
           </div>
         </div>
       </a>
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import PlanningListItem from '../components/PlanningListItem'
 import styles from '../styles/Home.module.css'
+import { useState } from 'react';
+
+const limit = 50;
+const distance = 500;
 
 export async function getServerSideProps(context) {
-  const limit = 50;
+  const whereQuery = `system_status not in('Final Decision', 'Withdrawn')`;
 
-  const res = await fetch(`${process.env.API_URL}.json?$limit=${limit}`)
+  const res = await fetch(`${process.env.API_URL}.json?$limit=${limit}&$where=${whereQuery}`)
   const data = await res.json()
 
   if (!data) {
@@ -17,13 +21,51 @@ export async function getServerSideProps(context) {
 
   return {
     props: {
+      apiUrl: process.env.API_URL,
       developments: data
     }
   }
 }
 
+const PlanningList = (props) => {
+  const { developments, currentLocation } = props;
+
+  return (
+    developments.map((d) => <PlanningListItem key={d.pk} development={d} currentLocation={currentLocation} />)
+  )
+}
+
 export default function Home(props) {
-  const { developments } = props;
+  const [developments, setDevelopments] = useState(props.developments);
+  const [postcode, setPostcode] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [currentLocation, setCurrentLocation] = useState();
+
+  const postcodeLookup = async (e) => {
+    e.preventDefault();
+
+    const postcodeRes = await fetch(`https://api.postcodes.io/postcodes/${postcode}`)
+    const postcodeData = await postcodeRes.json()
+
+    setError(postcodeData.error);
+
+    if (postcodeData.error) {
+      return
+    }
+
+    setLoading(true);
+    setPostcode(postcodeData.result.postcode);
+
+    const whereQuery = `system_status not in('Final Decision', 'Withdrawn') and within_circle(location, ${postcodeData.result.latitude}, ${postcodeData.result.longitude}, ${distance})`;
+
+    const res = await fetch(`${props.apiUrl}.json?$limit=${limit}&$where=${whereQuery}`)
+    const data = await res.json()
+
+    setCurrentLocation({ latitude: postcodeData.result.latitude, longitude: postcodeData.result.longitude });
+    setDevelopments(data);
+    setLoading(false);
+  }
 
   return (
     <div className={styles.container}>
@@ -47,11 +89,18 @@ export default function Home(props) {
             <p className={styles.description}>
               Find, review and leave feedback on open planning applications in Camden.
             </p>
+
+            <form onSubmit={postcodeLookup}>
+              <label htmlFor='postcode' className={styles.searchLabel}>Enter your postcode</label>
+              <input type='text' id='postcode' name='postcode' value={postcode} onChange={e => setPostcode(e.target.value)} className={styles.searchInput} />
+              { error ? <p>{ error }</p> : null }
+              <button>Search</button>
+            </form>
           </div>
         </div>
 
         <section className={styles.results}>
-          { developments.map((d) => <PlanningListItem key={d.pk} development={d}/>) }
+          { loading ? <p>Loading...</p> :  <PlanningList developments={developments} currentLocation={currentLocation} /> }
         </section>
       </main>
 

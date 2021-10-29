b53711b8f7f0b3dc621ba95e2b083186c881ccfc
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2021-10-29 17:15:35 +0100

    Add Netlify identity script

--- pages/index.js
+++ pages/index.js
@@ -89,6 +89,17 @@ export default function Home(props) {
         </section>
       </main>
 
+      <script>
+        if (window.netlifyIdentity) {
+          window.netlifyIdentity.on("init", user => {
+            if (!user) {
+              window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          })
+        }
+      </script>
     </div>
   )
 }

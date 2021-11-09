0e5a353ff76c8346de9ec710ff5c6325a4174e4f
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2021-11-09 10:41:50 +0000

    Update footer links

--- components/Footer.js
+++ components/Footer.js
@@ -8,20 +8,20 @@ const Footer = () => {
       <h5>Learn more</h5>
       <ul>
         <li>
-          <a href="" className={styles.footerItem}>
+          <a href="https://www.camden.gov.uk/camden-local-plan1" className={styles.footerItem} target='_blank' rel='noreferrer'>
             <span>Read Camden&apos;s Local Plan</span>
             <Image src="/icons/arrow-right-yellow.svg" alt="" width={8} height={14} />
           </a>
         </li>
         <li>
-          <a href="" className={styles.footerItem}>
-            <span>Application documents</span>
+          <a href="https://www.camden.gov.uk/planning-building-development" className={styles.footerItem} target='_blank' rel='noreferrer'>
+            <span>Planning guidance</span>
             <Image src="/icons/arrow-right-yellow.svg" alt="" width={8} height={14} />
           </a>
         </li>
         <li>
-          <a href="" className={styles.footerItem}>
-            <span>Useful links</span>
+          <a href="https://www.camden.gov.uk/planning-policy" className={styles.footerItem} target='_blank' rel='noreferrer'>
+            <span>Planning policies</span>
             <Image src="/icons/arrow-right-yellow.svg" alt="" width={8} height={14} />
           </a>
         </li>

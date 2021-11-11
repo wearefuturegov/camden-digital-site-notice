450c02d52ba1082529cedb08b1016994f0f7b361
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2021-11-11 18:01:48 +0000

    Add support for different color arrows, update TY page

--- components/BreadcrumbArrow.js
+++ components/BreadcrumbArrow.js
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import styles from '../styles/BreadcrumbArrow.module.css'
 
-export default function BreadcrumbArrow() {
+export default function BreadcrumbArrow({ colour }) {
   return (
     <span className={styles.arrow}>
-      <Image src="/icons/arrow-right-white.svg" alt="" width={8} height={12} />
+      <Image src={`/icons/arrow-right-${ colour || 'white'}.svg`} alt="" width={8} height={12} />
     </span>
   )
 }
--- pages/planning-applications/[id]/thank-you.js
+++ pages/planning-applications/[id]/thank-you.js
@@ -26,9 +26,7 @@ export default function ThankYou(props) {
           <BreadcrumbArrow colour='darkgreen' />
           <span className={styles.highlight}>Submit your feedback</span>
         </div>
-      </section>
 
-      <section className={styles.mainSection}>
         <h1 className={styles.heading}>Thank you, we&apos;re listening</h1>
         <p>
         Did you know you can have an even bigger impact by getting involved in <a href="https://www.camden.gov.uk/camden-local-plan1" className={styles.footerItem} target='_blank' rel='noreferrer'>Camden&apos;s Local Plan</a>?</p>

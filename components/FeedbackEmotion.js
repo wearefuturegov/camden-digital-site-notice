0f85aff7132e6633c9d20f182a366e5abaf018c0
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2021-11-12 09:38:46 +0000

    Don't lazy load emotion images

--- components/FeedbackEmotion.js
+++ components/FeedbackEmotion.js
@@ -5,7 +5,13 @@ export default function FeedbackEmotion({emotion, handleSelect, selected}) {
   return (
     <label className={styles.emotion}>
       <input type='radio' required className={styles.input} value={emotion} name='emotion' onChange={handleSelect} />
-      <Image src={`/icons/feedback/emotions/${emotion}${ selected ? '-selected' : ''}.svg`} alt={emotion} width='62' height='62' />
+
+      <Image
+        src={`/icons/feedback/emotions/${emotion}${ selected ? '-selected' : ''}.svg`}
+        alt={emotion}
+        width='62'
+        height='62'
+        priority={true} />
 
       <span className={styles.label}>{emotion}</span>
     </label>

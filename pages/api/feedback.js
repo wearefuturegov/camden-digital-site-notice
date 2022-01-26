2ec78e0ae8de965cea7f913477a4868037e9159d
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2022-01-26 15:28:00 +0100

    Use Sendgrid instead of Gmail for emails

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,46 +1,48 @@
-import nodemailer from 'nodemailer'
+import sendgrid from '@sendgrid/mail'
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { applicationNumber, feedbackEmotion, feedback, impactFeedback, postcode } = req.body;
     console.log(req.body);
 
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_APP_PASSWORD
-      }
-    });
+    sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
     const impactFeedbackHTML = impactFeedback ? impactFeedback.map(area => area.feedback ? `<h3>${area.name}</h3><p>${area.feedback}</p>` : null ).join('') : '';
 
-    const options = {
-      from: 'Camden Digital Site Notice',
-      to: process.env.GMAIL_USER,
-      subject: `Camden Digital Site Notice: New feedback for planning application ${applicationNumber}`,
+    const message = {
+      to: process.env.FEEDBACK_TO_EMAIL,
+      from: {
+        email: process.env.FEEDBACK_FROM_EMAIL,
+        name: 'Camden Digital Site Notice',
+      },
+      subject: `New feedback for planning application ${applicationNumber}`,
       html: `<h1>New feedback for planning application ${applicationNumber}</h1>
-            <h2>Feeling</h2>
-            <p>${feedbackEmotion}</p>
-            <h2>Feedback</h2>
-            <p>${feedback}</p>
-            ${ impactFeedbackHTML }
-            <h2>Postcode</h2>
-            <p>${postcode ? postcode : 'Not provided'}</p>`
+      <h2>Feeling</h2>
+      <p>${feedbackEmotion}</p>
+      <h2>Feedback</h2>
+      <p>${feedback ? feedback : 'Not provided'}</p>
+      ${ impactFeedbackHTML }
+      <h2>Postcode</h2>
+      <p>${postcode ? postcode : 'Not provided'}</p>`
     }
 
-    transporter.sendMail(options, (error) => {
-      if (error) {
-        console.log(error);
-        res.status(400).send({ message: error })
-      } else {
-        res.status(200).send({ message: 'Success' })
+    try {
+      await sendgrid.send(message)
+
+      res.status(200).send({ message: 'Success' })
+    } catch(error) {
+      console.error(error);
+
+      if (error.response) {
+        console.error(error.response.body)
+        res.status(400).send({ message: error.response.body })
       }
-    });
+
+      res.status(400).send({ message: error })
+    }
 
   } else {
     res.status(400).send({ message: 'Only POST requests allowed' })
-    return
   }
 
 }

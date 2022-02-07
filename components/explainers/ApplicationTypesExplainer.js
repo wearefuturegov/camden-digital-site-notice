2d9f8b024a64f2cede56b78a301ebfa39bc56e54
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2022-02-07 12:00:09 +0100

    Update application types explainer copy

--- components/explainers/ApplicationTypesExplainer.js
+++ components/explainers/ApplicationTypesExplainer.js
@@ -2,37 +2,27 @@ import Explainer from '../Explainer'
 
 export default function ApplicationTypesExplainer(props) {
   return <Explainer hasLightBg={props.hasLightBg} hasMarginTop={props.hasMarginTop} buttonText='Learn more about application types'>
-    <h2>How to write good&nbsp;feedback?</h2>
+    <h2>Learn more about application types</h2>
 
-    <h3>Influencing the finer details</h3>
+    <h3>Full planning application</h3>
     <p>
-      By the time a development has reached application stage, the developer has probably already bought the land and drawn up plans. Now it&apos;s about the details. Residents knowledge about local need can help planners decide things like type of housing or what materials are appropriate.
+      An application to alter, change the use of or construct a building. A full planning application provides all the details of the proposed development in one go.
     </p>
 
-    <h3>Eyes and ears on the ground</h3>
+    <h3>Outline planning application</h3>
     <p>
-      Residents have in-depth knowledge about their local area and often highlight on-the-ground details that planners weren&apos;t aware of, like how a view could be affected or how a tree was left out of a developer&apos;s plans. 
-      This is especially helpful now that planners aren&apos;t as likely to visit sites in person due to covid. Photos can be useful. 
+      An application seeking approval for the principle of the development — including size, layout, uses and key features. The rest of the details are provided later with a reserved matters applications.
     </p>
 
-    <h3>Provide back up on what&apos;s important</h3>
+    <h3>Reserved matters application</h3>
     <p>
-      Planners need bargaining power when negotiating with planners. It&apos;s useful to be able to demonstrate the certain priorities are important to residents so they can push developers to be more ambitious with their targets.
+      Provides the additional details that aren&apos;t provided in an outline planning application. This could include layout, materials and landscape details.
     </p>
 
-    <h3>Is this development good for the area?</h3>
+    <h3>Application for removal/variation of conditions</h3>
     <p>
-      For Major Developments, planners make a judgement on whether the development is good for the area. This is based on whether the building is compliant with planning legislation and regulations and how well the development contributes to the priorities set out in the local plan and the national planning framework.
+      Planning applications are often approved with ‘conditions’, which have to be delivered before, during or after construction. Applicants have to apply to change these conditions.
     </p>
 
-    <h3>What are the priorities?</h3>
-    <p>
-      A single development is unlikely to fulfill all the priorities for the local area. The planner decides which priorities matter most and then negotiates with the developer on targets.
-    </p>
-
-    <h3>Should this development be approved now?</h3>
-    <p>
-      A planning committee made up of elected councillors makes the final decision about whether a development is allowed to go ahead. Widespread objection from the public can slow down the decision.
-    </p>
   </Explainer>
 }

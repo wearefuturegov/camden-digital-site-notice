8b6a05150eccefed3a3a85c440a0d6ab9309b958
Harley Burrow <121872150+HarleyBurrowTPX@users.noreply.github.com>
2023-05-05 14:27:45 +0100

    WIP: 	- Added google tag manager and analytics scripts 	- Added no script 	- Google analytics only added if cookies accepted 	- Analytics and cookies removed if revoked 	- API Key stored as an environment variable (WIP)

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,25 @@
 import '../styles/globals.css';
 import * as gtag from '../lib/gtag';
+import Head from 'next/head';
+import { useState } from "react";
+
 
 function MyApp({ Component, pageProps }) {
+
+  const [apiKey, setApiKey] = useState(process.env.API_KEY);
   return (
     <>
+     <Head>
+        {/* Your other meta tags, title, stylesheets, and scripts */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WF28PJS"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+      </Head>
       <script
         src="https://cc.cdn.civiccomputing.com/9/cookieControl-9.x.min.js"
         strategy="afterInteractive"
@@ -13,7 +29,7 @@ function MyApp({ Component, pageProps }) {
         dangerouslySetInnerHTML={{
           __html: `
             var config = {
-              apiKey: 'API_KEY',
+              apiKey: "${apiKey}",
               product: 'community',
               optionalCookies: [
                 {
@@ -21,7 +37,7 @@ function MyApp({ Component, pageProps }) {
                   label: 'Analytics',
                   description: 'Analytical cookies help us to improve our website by collecting and reporting information on its usage.',
                   cookies: [],
-                  onAccept : function(){
+                  onAccept: function() {
                     // Enable Google Analytics
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
@@ -30,10 +46,24 @@ function MyApp({ Component, pageProps }) {
                       page_path: window.location.pathname,
                       cookie_flags: 'SameSite=None;Secure'
                     });
+                    
+                    // Enable Google Tag Manager
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-WF28PJS');
                   },
                   onRevoke: function(){
                     // Disable Google Analytics
                     window['ga-disable-${gtag.GA_TRACKING_ID}'] = true;
+
+                    // Disable Google Tag Manager
+                    var dataLayer = window.dataLayer || [];
+                    dataLayer.push({
+                      'event': 'gtm.disable',
+                      'gtm.start': new Date().getTime()
+                    });
                   }
                 },{
                   name: 'marketing',

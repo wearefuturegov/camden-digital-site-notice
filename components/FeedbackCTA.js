71952dac69c35f6613156f7d7b83eed5fa3b1ffc
Harley Burrow <121872150+HarleyBurrowTPX@users.noreply.github.com>
2023-05-15 14:26:38 +0100

    Fix multiple children passHref error

--- components/FeedbackCTA.js
+++ components/FeedbackCTA.js
@@ -4,11 +4,11 @@ import styles from '../styles/FeedbackCTA.module.css'
 
 export default function FeedbackCTA({id}) {
   return (
-    (<Link href={`/planning-applications/${id}/feedback`} className={styles.button}>
+    (<a href={`/planning-applications/${id}/feedback`} className={styles.button} passHref>
       We want your feedback!<div className={styles.arrow}>
         <Image src="/icons/arrow-right-yellow.svg" alt="" width={14} height={20} />
       </div>
 
-    </Link>)
+    </a>)
   );
 }

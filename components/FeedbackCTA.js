0a3f0b124f1a66b2532647b3b5f1f2ec89624a74
Harley Burrow <121872150+HarleyBurrowTPX@users.noreply.github.com>
2023-05-15 14:33:18 +0100

    Fix href error

--- components/FeedbackCTA.js
+++ components/FeedbackCTA.js
@@ -4,7 +4,7 @@ import styles from '../styles/FeedbackCTA.module.css'
 
 export default function FeedbackCTA({id}) {
   return (
-    (<a href={`/planning-applications/${id}/feedback`} className={styles.button} passHref>
+    (<a href={`/planning-applications/${id}/feedback`} className={styles.button} passhref="true">
       We want your feedback!<div className={styles.arrow}>
         <Image src="/icons/arrow-right-yellow.svg" alt="" width={14} height={20} />
       </div>

f4d3d702da29f04f32519c60ce0e585c28f6fdca
Harley Burrow <121872150+HarleyBurrowTPX@users.noreply.github.com>
2023-05-16 12:25:38 +0100

    Reconfiguring and simplifying cookie control scripts

--- pages/_app.js
+++ pages/_app.js
@@ -7,29 +7,19 @@ import loadCookieControl from 'components/CookieControl';
 
 console.log("API Key:", process.env.NEXT_PUBLIC_API_KEY);
 
-<MyApp apiKey={process.env.NEXT_PUBLIC_API_KEY} />
-
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
   useEffect(() => {
-    console.log("apiKey:", apiKey);
-    var config = {
-      apiKey: apiKey,
-      product: "community",
-      position: "BOTTOM",
-      theme: "DARK"
-    };
-    console.log("config:", config);
-    window.CookieControl.load(config);
+    console.log('apiKey:', apiKey);
+    loadCookieControl(apiKey);
   }, [apiKey]);
-
+ 
 
   return (
     <>
       <Head>
-        {/* Your other meta tags, title, stylesheets, and scripts */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-WF28PJS"
@@ -39,28 +29,7 @@ function MyApp({ Component, pageProps }) {
           ></iframe>
         </noscript>
       </Head>
-      <script
-        src="https://cc.cdn.civiccomputing.com/9/cookieControl-9.x.min.js"
-        strategy="afterInteractive"
-        async
-      />
-      
-      <script
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-        strategy="afterInteractive"
-        async
-      />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          `,
-        }}
-        strategy="afterInteractive"
-      />
-      <Component {...pageProps} {...loadCookieControl}/>
+      <Component {...pageProps} />
     </>
   );
 }

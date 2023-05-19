b94c3848d5dc2b6ca1ef17d9fd8fbffd21ab067d
Harley Burrow <121872150+HarleyBurrowTPX@users.noreply.github.com>
2023-05-19 11:17:03 +0100

    Reverting to Link

--- components/FeedbackCTA.js
+++ components/FeedbackCTA.js
@@ -4,11 +4,13 @@ import styles from '../styles/FeedbackCTA.module.css'
 
 export default function FeedbackCTA({id}) {
   return (
-    (<a href={`/planning-applications/${id}/feedback`} className={styles.button} passhref="true">
-      We want your feedback!<div className={styles.arrow}>
-        <Image src="/icons/arrow-right-yellow.svg" alt="" width={14} height={20} />
-      </div>
-
-    </a>)
-  );
+    <Link href={`/planning-applications/${id}/feedback`}>
+      <a className={styles.button}>
+        We want your feedback!
+        <div className={styles.arrow}>
+          <Image src="/icons/arrow-right-yellow.svg" alt="" width={14} height={20} />
+        </div>
+      </a>
+    </Link>
+  )
 }
